db81d6c4f037995a4a96527984567d1ddb152de6
valrus <imccowan@gmail.com>
2017-11-13 21:31:10 -0800

    space cleanup, add tufte parser

---
+++ mynt/parsers/tufte.py
@@ -0,0 +1,31 @@
+from itertools import chain
+
+import pypandoc
+
+from mynt.base import Parser as _Parser
+
+
+class Parser(_Parser):
+    accepts = ('.md', '.markdown')
+
+    def parse(self, markdown):
+        return pypandoc.convert_text(
+            markdown, 'html',
+            extra_args=self.flags,
+            format='markdown+smart+raw_tex+yaml_metadata_block',
+            filters=['pandoc-sidenote']
+        )
+
+    def setup(self):
+        self.css_styles = [
+            'tufte-css/tufte.css',
+            'tufte-pandoc-css/pandoc.css',
+            'tufte-pandoc-css/pandoc-solarized.css',
+            'tufte-pandoc-css/tufte-extra.css',
+        ]
+
+        self.flags = [
+            '--katex',
+            '--section-divs',
+            '--highlight-style=pygments',
+        ] + list(chain.from_iterable(['--css', css_style] for css_style in self.css_styles))
